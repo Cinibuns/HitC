802e59a564a5359bd4fd7d2a69f77b75a2f6e276
Matt Symons <mbgsymons@gmail.com>
2026-01-28 14:03:09 +1100

    Initial Commit

---
+++ HitC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HitC
+//
+//  Created by Matt Symons on 28/1/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HitC/HitCApp.swift
@@ -0,0 +1,17 @@
+//
+//  HitCApp.swift
+//  HitC
+//
+//  Created by Matt Symons on 28/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct HitCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
